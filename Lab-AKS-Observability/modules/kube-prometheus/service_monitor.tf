3dc186d4838c3a7341dd972444ccb4d53ddbe103
Wojciech Guzik <wojciech.guzik@gmail.com>
2025-01-22 22:17:22 +0100

    Readme, sprwadzenie krokow

--- Lab-AKS-Observability/modules/kube-prometheus/service_monitor.tf
+++ Lab-AKS-Observability/modules/kube-prometheus/service_monitor.tf
@@ -1,36 +1,36 @@
-resource "kubernetes_manifest" "default_service_monitor" {
-  manifest = {
-    apiVersion = "monitoring.coreos.com/v1"
-    kind       = "ServiceMonitor"
-    metadata = {
-      name      = "metrics-monitor"
-      namespace = "metrics"
-      labels = {
-        app = "metrics"
-      }
-    }
-    spec = {
-      selector = {
-        matchExpressions = [
-            {
-          key = "app"
-          operator = "Exists"
-        }
-        ]
-      }
-      endpoints = [
-        {
-          port   = "metrics"
-          path   = "/metrics"
-          scheme = "http"
-          interval = "15s"
-        }
-      ]
-      namespaceSelector = {
-        any = true
-      }
-    }
-  }
-
-  depends_on = [helm_release.kube-prometheus]
-}
+#resource "kubernetes_manifest" "default_service_monitor" {
+#  manifest = {
+#    apiVersion = "monitoring.coreos.com/v1"
+#    kind       = "ServiceMonitor"
+#    metadata = {
+#      name      = "metrics-monitor"
+#      namespace = "metrics"
+#      labels = {
+#        app = "metrics"
+#      }
+#    }
+#    spec = {
+#      selector = {
+#        matchExpressions = [
+#            {
+#          key = "app"
+#          operator = "Exists"
+#        }
+#        ]
+#      }
+#      endpoints = [
+#        {
+#          port   = "metrics"
+#          path   = "/metrics"
+#          scheme = "http"
+#          interval = "15s"
+#        }
+#      ]
+#      namespaceSelector = {
+#        any = true
+#      }
+#    }
+#  }
+#
+#  depends_on = [helm_release.kube-prometheus]
+#}
